81eba585fbf419c4cab5c96b318fb6ec729f1860
badri-neparidze <badri.neparidze@gmail.com>
2018-11-21 23:35:22 +0100

    [31]Extend Search bar with autocomplete

--- app/src/main/java/amos/corridornavigation/MainActivity.java
+++ app/src/main/java/amos/corridornavigation/MainActivity.java
@@ -4,9 +4,13 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -47,6 +51,7 @@ import timber.log.Timber;
 
 public class MainActivity extends MapContext
 {
+    public static String[] countries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +61,25 @@ public class MainActivity extends MapContext
 
         initMapView(savedInstanceState);
 
+        final AutoCompleteTextView addressSearchBar = (AutoCompleteTextView)
+                findViewById(R.id.main_searchbar_input);
+
+        addressSearchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                onSearchStart(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void actionButtonPressed(View view) {
@@ -66,6 +90,43 @@ public class MainActivity extends MapContext
         {
             Toast.makeText(this, R.string.user_location_not_available, Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void onSearchStart(CharSequence s) {
+        String addressPart = s.toString();
+        String[] dropdownSuggetions = getSuggestions(addressPart);
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.main_searchbar_input);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, dropdownSuggetions);
+        autoCompleteTextView.setAdapter(adapter);
+    }
+
+    // TOTO-make getSuggestion return list of suggested addresses, now it jsut returns
+    // hardcoded lsitof countries for demoing and testing.
+    private String[] getSuggestions(String addressPart) {
+        return new String[] {"Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua; Barbuda",
+                "Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh",
+                "Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina",
+                "Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia",
+                "Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands",
+                "Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark",
+                "Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea",
+                "Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia",
+                "French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland",
+                "Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras",
+                "Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel",
+                "Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos",
+                "Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau",
+                "Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius",
+                "Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia",
+                "Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger",
+                "Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru",
+                "Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda",
+                "Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia",
+                "Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain",
+                "Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland",
+                "Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga",
+                "Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine",
+                "United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"};
     }
 
     public void onSearchButtonClicked(View view) {
