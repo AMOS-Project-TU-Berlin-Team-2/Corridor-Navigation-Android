5e89d1b88304008a05acef5f9a9c4c3315680ac0
Zorxon <johann.l.moll@campus.tu-berlin.de>
2018-12-17 16:02:45 +0100

    Added reverse geocoding functionality; outputs to log atm

--- app/src/main/java/amos/corridornavigation/MapContext.java
+++ app/src/main/java/amos/corridornavigation/MapContext.java
@@ -3,12 +3,9 @@ package amos.corridornavigation;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mapbox.android.core.location.LocationEngine;
@@ -17,6 +14,8 @@ import com.mapbox.android.core.location.LocationEnginePriority;
 import com.mapbox.android.core.location.LocationEngineProvider;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.core.exceptions.ServicesException;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
@@ -24,11 +23,21 @@ import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.api.geocoding.v5.GeocodingCriteria;
+import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 
 import java.util.List;
-import java.util.Locale;
 
-public class MapContext extends AppCompatActivity implements LocationEngineListener, PermissionsListener, OnMapReadyCallback, MapboxMap.OnMapClickListener, MapboxMap.OnMapLongClickListener{
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
+
+public class MapContext extends AppCompatActivity implements LocationEngineListener, PermissionsListener, OnMapReadyCallback, MapboxMap.OnMapClickListener {
 
 
     protected MapView mapView;
@@ -43,14 +52,17 @@ public class MapContext extends AppCompatActivity implements LocationEngineListe
 
     protected Router locationMarker;
 
+    private MapboxGeocoding client = null;
+    //private TextView addressResultView;
+
 
     protected void initMapView(Bundle savedInstanceState) {
         locationMarker = new Router();
 
-        mapView = findViewById(R.id.navigationView);
+        mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
-
+        //addressResultView = findViewById(R.id.AddressResultView);
     }
 
 
@@ -65,29 +77,57 @@ public class MapContext extends AppCompatActivity implements LocationEngineListe
 
     @Override
     public void onMapClick(@NonNull LatLng point){
-
+        makeGeocodeSearch(point);
         locationMarker.setDestinationMarkerPosition(this, point);
     }
 
-    @Override
-    public void onMapLongClick(@NonNull LatLng point){
+    private void makeGeocodeSearch(LatLng latLng) {
+        try {
+            // Build a Mapbox geocoding request
+            client = MapboxGeocoding.builder()
+                    .accessToken(getString(R.string.access_token))
+                    .query(Point.fromLngLat(latLng.getLongitude(), latLng.getLatitude()))
+                    .geocodingTypes(GeocodingCriteria.TYPE_ADDRESS)
+                    .build();
 
-        LinearLayout search_navi_ui = (LinearLayout) findViewById(R.id.search_navi_ui);
-        FloatingActionButton hideButton = findViewById(R.id.hide_button);
-        FloatingActionButton compassButton = findViewById(R.id.floatingActionButton2);
+            client.enqueueCall(new Callback<GeocodingResponse>() {
+                @Override
+                public void onResponse(Call<GeocodingResponse> call,
+                                       Response<GeocodingResponse> response) {
+                    List<CarmenFeature> results = response.body().features();
+                    if (results.size() > 0) {
 
-        if (search_navi_ui.getVisibility()== View.VISIBLE){
-            search_navi_ui.setVisibility(View.INVISIBLE);
-            hideButton.hide();
-            compassButton.hide();
-        }
-        else {
-            search_navi_ui.setVisibility(View.VISIBLE);
-            hideButton.show();
-            compassButton.show();
+                        // Get the first Feature from the successful geocoding response
+                        CarmenFeature feature = results.get(0);
+                        //System.out.println(feature.toString());
+                        Log.d("Place Name", feature.placeName());
+                        //addressResultView.setText(feature.placeName());
+
+                        animateCameraToNewPosition(latLng);
+                    } else {
+                        Toast.makeText(MapContext.this, "No results found.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+                    Timber.e("Geocoding Failure: " + throwable.getMessage());
+                }
+            });
+        } catch (ServicesException servicesException) {
+            Timber.e("Error geocoding: " + servicesException.toString());
+            servicesException.printStackTrace();
         }
     }
 
+    private void animateCameraToNewPosition(LatLng latLng) {
+        mapboxMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(new CameraPosition.Builder()
+                        .target(latLng)
+                        .zoom(13)
+                        .build()), 1500);
+    }
 
     @SuppressWarnings( {"MissingPermission"})
     private void enableLocationPlugin() {
@@ -155,7 +195,6 @@ public class MapContext extends AppCompatActivity implements LocationEngineListe
     public void onMapReady(MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
         mapboxMap.addOnMapClickListener(this);
-        mapboxMap.addOnMapLongClickListener(this);
         enableLocationPlugin();
     }
 
@@ -202,6 +241,9 @@ public class MapContext extends AppCompatActivity implements LocationEngineListe
 
     @Override
     protected void onDestroy() {
+        if (client != null) {
+            client.cancelCall();
+        }
         super.onDestroy();
         mapView.onDestroy();
         if(locationEngine != null)
